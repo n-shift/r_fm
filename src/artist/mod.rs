278e9312cdde9cf41d82e71249eee8ef48945fb9
shift-d <shiftkeydeveloper@gmail.com>
2024-12-22 20:40:36 +0300

    change Opts signature

--- src/artist/mod.rs
+++ src/artist/mod.rs
@@ -1,6 +1,6 @@
 mod raw;
 use crate::from_raw;
-use crate::shared::SizedImages;
+use crate::shared::{SizedImages, Opts};
 use raw::Bio;
 use raw::SUsize;
 
@@ -52,37 +52,18 @@ impl Spec {
     }
 }
 
-#[derive(Default)]
-pub struct Opts {
-    pub lang: Option<String>,
-    pub autocorrect: bool,
-    pub username: Option<String>,
-}
-
 pub struct Artist<'a>(pub Spec, pub &'a str);
 impl Artist<'_> {
-    pub async fn get_info(&self, client: &Client, opts: Opts) -> anyhow::Result<ArtistInfo> {
+    pub async fn get_info(&self, client: &Client, opts: Opts<'_>) -> anyhow::Result<ArtistInfo> {
         ArtistInfo::get(client, self.0, self.1, opts).await
     }
 }
 
 use super::Client;
 impl ArtistInfo {
-    async fn get(client: &Client, spec: Spec, val: &str, opts: Opts) -> anyhow::Result<Self> {
+    async fn get(client: &Client, spec: Spec, val: &str, opts: Opts<'_>) -> anyhow::Result<Self> {
         let dpar = &[("method", "artist.getInfo"), (spec.to_param(), val)];
-        let mut add = Vec::new();
-        let lang = opts.lang.unwrap_or("".into());
-        let user = opts.username.unwrap_or("".into());
-        if opts.autocorrect {
-            add.push(("autocorrect", "1"));
-        }
-        if !lang.is_empty() {
-            add.push(("lang", lang.as_str()));
-        }
-        if !user.is_empty() {
-            add.push(("username", user.as_str()));
-        }
-        let params = &[dpar, add.as_slice()].concat();
+        let params = &[dpar, opts.unwrap_or(&[])].concat();
         let info: ArtistInfo = client
             .build(params)
             .send()
--- src/shared.rs
+++ src/shared.rs
@@ -1,2 +1,3 @@
 use std::collections::HashMap;
 pub type SizedImages = HashMap<String, String>;
+pub type Opts<'a> = Option<&'a [(&'a str, &'a str)]>;
